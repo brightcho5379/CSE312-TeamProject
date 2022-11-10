e36a14b8a23bfe82bf668d0cb4fcc0e73044fb3b
camerontoy <cameron.j.toy@gmail.com>
2022-11-10 03:04:30 -0500

    Implment MongoDB

---
+++ back-end/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+//Routes
+app.use('/user',require('./routes/login.js'))
+app.use('/',require('./routes/login.js'))
+
+const URI = process.env.DATABASE_URI
+mongoose.connect(URI, () =>
+    console.log('Connected to MongoDB')
+)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () =>{
+    console.log('Server is running on port', PORT)
+})
+
+
