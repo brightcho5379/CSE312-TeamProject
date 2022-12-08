af240b101b746d523de5a07af193b74cb95f30e7
Jason Mai <maijason112102@gmail.com>
2022-12-07 20:19:20 -0500

    Connected front-end to back-end login

--- front-end/src/components/Login/Login.jsx
+++ front-end/src/components/Login/Login.jsx
@@ -4,34 +4,63 @@ import './Login.css'
 import axios from 'axios'
 
 const Login = () => {
-    const [user, setUser] = useState({
-        email:'', password: ''
-    })
 
-    const onChangeInput = e =>{
-        const {name, value} = e.target;
-        setUser({...user, [name]:value})
-    }
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        console.log(email, password);
 
-    const loginSubmit = async e =>{
-        e.preventDefault()
         try {
-            await axios.post('/user/login', {...user})
-            window.location.href = "/";
-        } catch (err) {
-            alert(err.response.data.msg)
+            const config = {
+                headers: {
+                    "Content-type":"application/json"
+                }
+            }
+            setLoading(true);
+            const {data} = await axios.post('/user/login', {
+                email,
+                password,
+            },
+            config
+            );
+            console.log(data);
+            localStorage.setItem('userInfo', JSON.stringify(data));
+            setLoading(false);
+        } catch (error) {
+            setError(error.response.data.message);
         }
     }
+    // const [user, setUser] = useState({
+    //     email:'', password: ''
+    // })
+
+    // const onChangeInput = e =>{
+    //     const {name, value} = e.target;
+    //     setUser({...user, [name]:value})
+    // }
+
+    // const loginSubmit = async e =>{
+    //     e.preventDefault()
+    //     try {
+    //         await axios.post('/user/login', {...user})
+    //         window.location.href = "/";
+    //     } catch (err) {
+    //         alert(err.response.data.msg)
+    //     }
+    // }
 
     return (
         <div className="login-page">
-            <form onSubmit={loginSubmit}>
+            <form onSubmit={submitHandler}>
                 <h2>Login</h2>
                 <input type="email" name="email" required
-                placeholder="Email" value={user.email} onChange={onChangeInput} />
+                placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 <br/>
                 <input type="password" name="password" required autoComplete="on"
-                placeholder="Password" value={user.password} onChange={onChangeInput} />
+                placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
 
                 <div className="Login-Button">
                     <button type="submit">Login</button>
