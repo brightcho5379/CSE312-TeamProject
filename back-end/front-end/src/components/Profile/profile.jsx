325254a89a23778b0c29fe5c89852b9a0de5344c
Jason Mai <maijason112102@gmail.com>
2022-12-09 20:12:58 -0500

    Auction (FE) and Add to Cart
    Add to Cart button on Home Page to send data to back-end.
    
    Auction Front-end is accomplished.

--- back-end/front-end/src/components/Profile/profile.jsx
+++ back-end/front-end/src/components/Profile/profile.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 
 export default class UserProfile extends Component {
   state = {
-    name: 'John Doe',
-    email: 'john.doe@example.com',
+    name: 'Jason Mai',
+    email: 'jtmai2@buffalo.edu',
     orderHistory: []
   }
 
